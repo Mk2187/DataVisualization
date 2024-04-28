6824ce80fdcaa2551ecccec78094c230c73039be
Mk2187 <78743326+Mk2187@users.noreply.github.com>
2024-04-28 23:51:00 +0530

    TCS data updloadeds

--- data_from_yf.py
+++ data_from_yf.py
@@ -2,7 +2,8 @@
 import yfinance as yf
 
 # Get the data
-data = yf.download(tickers="MSFT", period="500d", interval="1D")
+data = yf.download(tickers="TCS", period="500d", interval="1D")  #for day intervel
+#data = yf.download(tickers="MSFT", period="5d", interval="1m")    #1 minute interval
 print(data.head(5))
 data.columns
-data.to_csv("dta_from_yf.csv")
+#data.to_csv("data_from_yf.csv")
--- data_plot.py
+++ data_plot.py
@@ -1,1 +1,12 @@
 print("Hello World")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt  #pip install matplotlib
+import matplotlib as mpl
+#%matplotlib inline
+#%matplotlib widget
+df =pd.read_csv("data_from_yf.csv")
+df.set_index(df['Date'])
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot(df['Date'], df['Close'])  # Plot some data on the axes.
+plt.show()
